ce818ed7ab1811e57110aa3f2075511e2c0db341
gabsoares <soaresgabriel68@gmail.com>
2024-04-20 01:07:47 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioCalcularPorcentagemAumento/Program.cs
@@ -0,0 +1,16 @@
+﻿double salarioInicial = 2, salarioFinal = 1, porcentagemAumento = 0, aumento = 0;
+
+while (salarioInicial > salarioFinal)
+{
+    Console.Write("Digite o valor do salário: ");
+    salarioInicial = double.Parse(Console.ReadLine());
+
+    Console.Write("Digite o valor do salário com acréscimo (não pode ser valor menor que o salário inicial): ");
+    salarioFinal = double.Parse(Console.ReadLine());
+
+    aumento = salarioFinal - salarioInicial;
+
+    porcentagemAumento = aumento * 100 / salarioInicial;
+}
+
+Console.WriteLine($"O aumento foi de {porcentagemAumento}%");
